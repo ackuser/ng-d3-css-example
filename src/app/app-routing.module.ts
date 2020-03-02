5f05aa085b0b7effab3e2bd18157a20513e88346
Karim Abdeselam <karim.abdeselam@pragsis.com>
2020-03-02 14:04:27 +0100

    Creating Basic Scaffolding for d3 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { LineChartComponent } from './line-chart/line-chart.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LineChartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as d3 from "d3";
+
 
 @Component({
   selector: 'app-line-chart',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineChartComponent implements OnInit {
 
+  @ViewChild("chart", { static: true }) protected chartContainer: ElementRef;
+  svg: any;
+  tooltip: any;
+  margin: { top: number; right: number; bottom: number; left: number; };
+  contentWidth: number;
+  contentHeight: number;
+  width: number;
+  height: number;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initChart();
+
   }
+
+  initChart() {
+    const element = this.chartContainer.nativeElement;
+
+    this.svg = d3.select(element)
+      .attr("width", "100%")
+      .attr("height", "100%"),
+
+      this.margin = {
+        top: +this.svg.style("margin-top").replace("px", ""),
+        right: +this.svg.style("margin-right").replace("px", ""),
+        bottom: +this.svg.style("margin-bottom").replace("px", ""),
+        left: +this.svg.style("margin-left").replace("px", "")
+      },
+      this.width = +this.svg.style("width").replace("px", ""),
+      this.height = +this.svg.style("height").replace("px", ""),
+      this.contentWidth = this.width - this.margin.left - this.margin.right,
+      this.contentHeight = this.height - this.margin.top - this.margin.bottom - 20;
+  }
+
 
 }
